5baf7ad681ac80b6e725be761eab5c35051534f9
Adrian <avjpl@aol.com>
2014-02-08 10:06:09 +0000

    Backbone app skeleton start

---
+++ config.rb
@@ -0,0 +1,12 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "img"
+javascripts_dir = "js"
+font_dir = "css/fonts"
+
+output_style = :nested
+
+relative_assests = true
+
+line_comments = true
